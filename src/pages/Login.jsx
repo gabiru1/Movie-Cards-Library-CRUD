9f23888e0e915f53e9e23fcaf87c9a75c9fc319f
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-15 22:20:15 -0300

    requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userLogged: false,
+      loading: false,
+      name: '',
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ name: value });
+  }
+
+  signIn = () => {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    createUser({ name })
+      .then(() => this.setState({ userLogged: true, loading: false }));
+  }
+
   render() {
+    const { name, userLogged, loading } = this.state;
+    const minValueSize = 3;
+    if (userLogged) {
+      return (
+        <Redirect to="/search" />
+      );
+    }
     return (
       <div data-testid="page-login">
-        <input
-          type="text"
-          data-testid="login-name-input"
-        />
+        {loading
+          ? <Loading />
+          : (
+            <div>
+              <input
+                value={ name }
+                type="text"
+                data-testid="login-name-input"
+                onChange={ this.handleChange }
+              />
+              <button
+                disabled={ name.length < minValueSize }
+                type="button"
+                data-testid="login-submit-button"
+                onClick={ this.signIn }
+              >
+                Entrar
+              </button>
+            </div>
+          )}
       </div>
     );
   }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found">
-        teste
-      </div>
+      <div data-testid="page-not-found" />
     );
   }
 }
