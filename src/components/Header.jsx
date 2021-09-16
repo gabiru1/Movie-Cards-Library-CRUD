c10188b997801b235da2dd0510f414bdf9a29f43
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-16 11:49:41 -0300

    importa Link apos React

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
-import { Link } from 'react-router-dom';
 
 class Header extends Component {
   constructor(props) {
