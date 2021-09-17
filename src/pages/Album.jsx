b126a6706fe97509da4a7ce76f9194b6809b0eec
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-17 13:19:08 -0300

    requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -7,25 +7,34 @@ class Album extends Component {
     super(props);
 
     this.state = {
-      music: [], // array de musicas de um album
+      allMusics: [], // array de musicas de um album
       requestEnd: false, // Seta o estado da requisicao como false para fazer a logica da funcao que requisita
     };
   }
 
   componentDidMount() {
     const { match: { params: { id } } } = this.props; // Silveira/Kauan monstros!!!!
-    getMusics(id).then((musicData) => {
-      console.log(musicData);
-      this.setState({ music: musicData, requestEnd: true });
+    console.log(this.props);
+    getMusics(id).then((musicData) => { // filtra o album clicado
+      console.log(musicData); // array de musicas do album clicado
+      this.setState({ allMusics: musicData, requestEnd: true });
     });
   }
+  // a posição 0 do array allMusics não é uma música, apenas as informações do album
 
   render() {
-    const { music, requestEnd } = this.state;
+    const { allMusics, requestEnd } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        {console.log(music)}
+        {requestEnd
+          ? (
+            <div>
+              <p data-testid="artist-name">{ allMusics[0].artistName }</p>
+              <p data-testid="album-name">{ allMusics[0].collectionName }</p>
+            </div>
+          )
+          : ''}
       </div>
     );
   }
